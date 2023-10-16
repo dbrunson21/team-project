daee12713ddecb2c77580e8048fdaf555b771324
Chris.Openshaw <copenshaw17@gmail.com>
2023-10-16 11:25:20 -0500

    alot of changes lol

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,29 @@
 const apiKey = 'cca843b0a3msh3bae8af4ca13636p1832e0jsnf296ec4e2a77'
 const movieLists = ["most_pop_movies", "most_pop_series", "top_rated_series_250"]
 
-async function fetchRecipe() {
+function generate() {
+  let movieSelect = document.getElementById("movie-select")
+  let recipeSelect = document.getElementById("recipe-select")
+  movieSelect.setAttribute("style", "background-color: rgb(169, 169, 169);")
+  recipeSelect.setAttribute("style", "background-color: rgb(169, 169, 169);")
+
   let cuisine = document.getElementById("recipe-drp-dwn").value
+  let listIndex = Math.floor(Math.random() * movieLists.length)
+  let listType = movieLists[listIndex]
+  let genre = document.getElementById("movie-drp-dwn").value
+
+  if (genre === "none") {
+    movieSelect.setAttribute("style", "background-color: rgb(255, 0, 0, .8);")
+  }
+  if (cuisine === "none") {
+    recipeSelect.setAttribute("style", "background-color: rgb(255, 0, 0, .8);")
+  } else {
+    fetchRecipe(cuisine)
+    fetchMovie()
+  }
+}
+  
+async function fetchRecipe(cuisine) {
   let url = `https://edamam-recipe-search.p.rapidapi.com/api/recipes/v2?type=public&cuisineType=${cuisine}`
   const options = {
     method: 'GET',
@@ -27,25 +48,28 @@ async function fetchRecipe() {
     ///image = recipeDetails.image
     ///link = recipeDetails.link
     ///cook time = recipeDetails.time
-    let recipeDetails = {name:selectedRecipe.label, calories:selectedRecipe.calories, image:selectedRecipe.image, link:selectedRecipe.url, time:selectedRecipe.totalTime}
+    let recipeDetails = [selectedRecipe.label, selectedRecipe.calories, selectedRecipe.image, selectedRecipe.url, selectedRecipe.totalTime]
 
-    console.log(selectedRecipe, recipeDetails)
+    let recipeImage = document.getElementById("recipe-image")
+    recipeImage.setAttribute("src", recipeDetails[2])
+    recipeImage.style.visibility = "visible";
   } catch (error) {
     console.error(error);
   }
 }
 
+
 async function fetchMovie() {
   let listIndex = Math.floor(Math.random() * movieLists.length)
   let listType = movieLists[listIndex]
   let genre = document.getElementById("movie-drp-dwn").value
-  const url = `https://moviesdatabase.p.rapidapi.com/titles/random?genre=${genre}&list=${listType}`;
+  const url = `'https://moviesminidatabase.p.rapidapi.com/movie/byGen/${genre}/'`;
   const options = {
-	  method: 'GET',
-	  headers: {
-		  'X-RapidAPI-Key': 'cca843b0a3msh3bae8af4ca13636p1832e0jsnf296ec4e2a77',
-		  'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-	  }
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'cca843b0a3msh3bae8af4ca13636p1832e0jsnf296ec4e2a77',
+      'X-RapidAPI-Host': 'moviesminidatabase.p.rapidapi.com'
+    }
   };
 
   try {
@@ -53,56 +77,19 @@ async function fetchMovie() {
 	  const result = await response.json();
 	  console.log(result);
 
-    let movies = result.results
-    let movieIndex = Math.floor(Math.random() * movies.length)
-    let selectedMovie = movies[movieIndex]
-    
-    let movieDetails = {title:selectedMovie.titleText.text, image:selectedMovie.primaryImage.url, year:selectedMovie.releaseYear.year}
-    console.log(movieDetails)
+    //let movies = result.results
+    //let movieIndex = Math.floor(Math.random() * movies.length)
+    //let selectedMovie = movies[movieIndex]
+    //
+    //let movieDetails = [selectedMovie.titleText.text, selectedMovie.primaryImage.url, selectedMovie.releaseYear.year]
+
+   //let movieImage = document.getElementById("movie-image")
+   //movieImage.setAttribute("src", movieDetails[2])
+   //movieImage.style.visibility = "visible";
   } catch (error) {
 	  console.error(error);
   }
 }
-
-const recipeButton = document.getElementById("recipe-btn");
-const movieButton = document.getElementById("movie-btn");
-
-recipeButton.addEventListener("click", fetchRecipe);
-movieButton.addEventListener("click", fetchMovie);
-
-async function fetchImages() {
-  let url = `https://edamam-recipe-search.p.rapidapi.com/api/recipes/v2?type=public`
-  const options = {
-    method: 'GET',
-    headers: {
-      'Accept-Language': 'en',
-      'X-RapidAPI-Key': recipeAPI,
-      'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
-    }
-  };
-  
-  try {
-    const response = await fetch(url, options);
-    const result = await response.json();
-
-    let recipes = result.hits
-    let indexes = []
-    
-    while (indexes.length < 5) {
-      let index = Math.floor(Math.random() * recipes.length)
-      if (indexes.includes(index)) {
-        null
-      } else {
-        indexes.push(index)
-      }
-    }  
-    console.log(indexes)
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-document.addEventListener("load", fetchImages)
 
 $(document).ready(function () {
   $(".carousel").carousel({
